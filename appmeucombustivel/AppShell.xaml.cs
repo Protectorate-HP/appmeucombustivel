63f34a6d1984d50cbc0be5f51b543a307f3cf79f
Sukinhu <Sukinhu@IMPERIOHP>
2024-10-13 16:41:11 -0300

    Adicionar arquivos de projeto.

---
+++ appmeucombustivel/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace appmeucombustivel
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ appmeucombustivel/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace appmeucombustivel
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double etanol = Convert.ToDouble(txt_etanol.Text);
+                double gasolina = Convert.ToDouble(txt_gasolina.Text);
+                String msg = "";
+
+                if (etanol <= (gasolina * 0.70))
+                {
+                    msg = "o etanol esta compensando mais";
+                }
+                else
+                {
+                    msg = "a gasolina esta compensando mais";
+                }
+                DisplayAlert("Calculado", msg, "Ok");
+            } catch (Exception ex)
+            {
+                DisplayAlert("Aff", ex.Message, "Fechar");
+            }
+
+        }
+    }
+}
